cfea34c89935ee37281c5982119f148f4509d4d2
Paul Ollis <paul@cleversheep.org>
2020-10-26 17:57:56 +0000

    Working on the release.

--- admin/mk-release.py
+++ admin/mk-release.py
@@ -24,7 +24,7 @@ def generate():
     with zipfile.ZipFile(
             'vim-vpe.zip', mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
         os.chdir('..')
-        for sub_dir in ('docs/html', 'start', 'vpe'):
+        for sub_dir in ('examples', 'start', 'vpe'):
             subdir_path = root / 'vim-vpe'/ sub_dir
             for path in walk_relpaths(subdir_path):
                 relpath = path.relative_to(root)
