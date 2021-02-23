2e96775b46b596a4be9d2e568ee33986e24d337b
Paul Ollis <paul@cleversheep.org>
2021-02-23 20:03:30 +0000

    Add app_ui_support module.
    An independent feature that provides information at the Vim
    application's user interface - display sizes, Vim windows size.

---
+++ test/test_app_ui_support.py
@@ -0,0 +1,54 @@
+"""Special Vim application user interface support.
+
+This provides special support for the overall Vim application user interface.
+It only works on X.Org based desktops and then only if the correct programs
+are available.
+"""
+
+import platform
+
+# pylint: disable=unused-wildcard-import,wildcard-import
+from cleversheep3.Test.Tester import *
+from cleversheep3.Test.Tester import test, runModule
+
+import support
+
+import vpe
+
+_run_after = ['test_vim.py']
+
+
+class PanelViewBuffer(support.Base):
+    """VPE support organised display within a ScratchBuffer."""
+
+    @test(testID='app-ui-query')
+    def app_ui_query(self):
+        """Details about the application's UI can be queried.
+
+        :<py>:
+
+            from vpe import app_ui_support
+
+            res = Struct()
+
+            displays = app_ui_support.get_display_info()
+            win = app_ui_support.get_app_win_info()
+            res.displays = displays.displays
+
+            display = displays.find_display_for_window(win)
+            res.display_x = display.x
+
+            res.decor_dims = win.decor_dims
+            res.columns = win.columns
+
+            dump(res)
+        """
+        res = self.run_self()
+        failUnless(isinstance(res.displays, list))
+        failUnless(isinstance(res.display_x, int))
+        failUnless(isinstance(res.decor_dims, tuple))
+        failUnless(98 <= res.columns <= 104)
+
+
+if __name__ == '__main__':
+    runModule()
---
+++ vpe/app_ui_support.py
@@ -0,0 +1,203 @@
+"""Application level user interface support.
+
+Currently this only works on X.Org based desktops.
+"""
+
+import re
+import subprocess
+from functools import cache
+from typing import Optional, Tuple
+
+from vpe import vim
+
+R_COORD = re.compile(r'([+-]-?\d+)([+-]-?\d+)')
+R_GEOM = re.compile(r'(\d+)x(\d+)([+-]-?\d+)([+-]-?\d+)')
+R_DIMS = re.compile(r'(\d+)x(\d+)')
+
+
+def _system(cmd):
+    """Simple wrapper of subprocess to provide."""
+    proc = subprocess.run(cmd.split(), capture_output=True)
+    return proc.returncode, proc.stdout.decode(errors='ignore')
+
+
+# VimRegistry(STRING) = "1a00003 GVIM1", "2800003 GVIM2", "4800003 GVIM", "4400003 TEST"
+
+@cache
+def _my_xwin_id():
+    exitcode, text = _system('xprop -root VimRegistry')
+    if exitcode != 0:
+        return ''                                            # pragma: no cover
+    my_servername = vim.vvars.servername
+    _, _, text = text.partition(' = ')
+    for entry in text.split(', '):
+        try:
+            wid, name = entry.strip()[1:-1].split()
+        except ValueError:                                   # pragma: no cover
+            continue
+        if name == my_servername:
+            return f'0x{wid}'
+    return ''                                                # pragma: no cover
+
+
+class _Coord:
+    """A simple X/ Y coordinate."""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class AppWin:
+    """Information about Vim's application window.
+
+    :@dims_pixels:  A sequence (w, h) giving the window's undecorated size in
+                    pixels.
+    :@dims_cells:   A sequence (w, h) giving the window's undecorated size in
+                    character cells.
+    :@dims_corners: A sequence of pixel coordiates for the windows corners, in
+                    the order TL, TR, BR, BL. For TR and BR the X value is with
+                    repect to the right hand edge of the display. For BL and BR
+                    the Y value is with repect to the lower edge of the
+                    display.
+    :@borders:      The pixel sizes of the window decoration borders in the
+                    order, left, right, top, bottom.
+    :@cell_size:    The size of a character cell, in pixels.
+    """
+    def __init__(
+            self, dims_pixels, dims_cells, corners, borders, cell_size):
+        self.dims_pixels = dims_pixels
+        self.dims_cells = dims_cells
+        self.corners = corners
+        self.borders = borders
+        self.cell_size = cell_size
+
+    @property
+    def columns(self):
+        """The calculated number of columns for this window.
+
+        This should be the same as the columns option value.
+        """
+        return self.dims_pixels[0] // self.cell_size[0]
+
+    @property
+    def decor_dims(self) -> Tuple[int, int]:
+        """The windows dimension in pixels including window decoration."""
+        a, b, c, d = self.borders
+        w, h = self.dims_pixels
+        return w + a + b, h + c + d
+
+
+class Display:
+    """Information about a single display (physical screen).
+
+    :@w: The width in pixels.
+    :@h: The height in pixels.
+    :@x: The X coordinate, in pixels, of the top left corner.
+    :@y: The Y coordinate, in pixels, of the top left corner.
+    """
+    def __init__(self, w, h, x, y):
+        self.x, self.y = x, y
+        self.w, self.h = w, h
+
+    def contains_window(self, w):
+        c = w.corners[0]
+        if not self.x <= c.x < self.x + self.w:
+            return False                                     # pragma: no cover
+        if not self.y <= c.y < self.y + self.h:
+            return False                                     # pragma: no cover
+        return True
+
+
+class Displays:
+    """Information about the available displays (physical screens).
+
+    @displays: A sequence of `Display` instances.
+    """
+    def __init__(self):
+        self.displays = []
+
+    def add(self, display):
+        """Add a display."""
+        self.displays.append(display)
+
+    def find_display_for_window(self, w: AppWin) -> Optional[Display]:
+        """Find which display a given `Window` is on.
+
+        The position of the windows top-left corner is used for the
+        determination.
+
+        :w: The window bein searched for.
+        """
+        for display in self.displays:
+            if display.contains_window(w):
+                return display
+        return None                                          # pragma: no cover
+
+
+def get_display_info() -> Displays:
+    """Get information about the displays (screens)."""
+
+    _, text = _system('xrandr')
+    displays = Displays()
+    for line in text.splitlines():
+        line = line.strip()
+        if ' connected ' in line:
+            m = R_GEOM.search(line)
+            args = [int(g) for g in m.groups()]
+            displays.add(Display(*args))
+    if displays.displays:
+        return displays
+
+    # Likely the xrandr command was not available.
+    return None                                              # pragma: no cover
+
+
+def get_app_win_info() -> Optional[AppWin]:
+    """Get information about the Vim application window."""
+
+    wid = _my_xwin_id()
+    if not wid:
+        return None                                          # pragma: no cover
+
+    _, text = _system(f'xwininfo -id {wid}')
+    print('>>>>', repr(text))
+    print(text)
+    for line in text.splitlines():
+        line = line.strip()
+        if line.startswith('Width'):
+            w_pixels = int(line.split()[-1])
+        if line.startswith('Height'):
+            h_pixels = int(line.split()[-1])
+        if line.startswith('-geometry'):
+            m = R_GEOM.search(line)
+            dims_cells = int(m.group(1)), int(m.group(2))
+        if line.startswith('Corners'):
+            corners = [_parse_corner(c) for c in line.split()[1:]]
+
+    _, text = _system(
+        f'xprop -id {wid} _NET_FRAME_EXTENTS WM_NORMAL_HINTS')
+    print('>>>>', repr(text))
+    print(text)
+    for line in text.splitlines():
+        line = line.strip()
+        if line.startswith('_NET_FRAME_EXTENTS(CARDINAL) = '):
+            values = line.split('=')[-1].split(',')
+            borders = [int(v.strip()) for v in values]
+        if line.startswith('program specified resize increment: '):
+            values = line.split(':')[-1].split(' by ')
+            cell_size = [int(v.strip()) for v in values]
+
+    try:
+        return AppWin((
+            w_pixels, h_pixels), dims_cells, corners, borders,
+            cell_size)
+    except NameError:                                        # pragma: no cover
+        # Likely one of the commands was not available.
+        return None
+
+
+def _parse_corner(text):
+    """Parse information about the window's corners."""
+    m = R_COORD.match(text)
+    xs, ys = [s.replace('--', '') for s in m.groups()]
+    return _Coord(int(xs), int(ys))
